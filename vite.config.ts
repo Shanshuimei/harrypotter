e6cf1a12dc665e772c657865345e0774d262d4a5
Shanshuimei <1316317715@qq.com>
2025-02-05 16:26:07 +0800

    更新 GitHub Actions 工作流以支持静态内容部署，并修改 Vite 配置以设置基础路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // 设置基础路径为相对路径
+  base: '/harrypotter', 
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'], // 确保所有图片类型都被正确处理
   plugins: [react()],
   optimizeDeps: {
